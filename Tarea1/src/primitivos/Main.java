f2277904f9878f3f777c81e78b048f061d9af3d9
Jose Roberto Perez Angulo <jperez@g-ventura.com>
2023-02-12 10:09:02 -0600

    Apuntes de curso

---
+++ Tarea1/src/primitivos/Main.java
@@ -0,0 +1,10 @@
+package primitivos;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int numero =  1;
+        System.out.printf("numero de numero: " + numero);
+
+    }
+}
